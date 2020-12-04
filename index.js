e19de9917747efb081e63f6ec0827b4217c2ec66
John Quinzon <34043705+jzquinzon@users.noreply.github.com>
2020-12-04 01:18:00 -0800

    make things better

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const router = express.Router();
 
 const fetch = require('node-fetch');
 
-//restaurantApiRequest(' tapioca express', "", "", "Convoy San Diego");
+restaurantApiRequest(' tapioca express', "", "", "Convoy San Diego");
+//restaurantByIdApiRequest('WavvLdfdP6g8aZTtbBQHTw');
 
+/***
+ * https://developer.edamam.com/edamam-docs-recipe-api
+ */
 function recipeApiRequest(query) {
     let headers = {
         'Content-Type': 'application/x-www-form-urlencoded',
@@ -64,6 +68,9 @@ function recipeApiRequest(query) {
     })
 }
 
+/***
+ * https://www.yelp.com/developers/documentation/v3/business_search
+ ***/
 function restaurantApiRequest(term, latitude, longitude, location) {
     let clientID = 'rII8Y9mwHKyzKVlEWGQ2QA';
     let apiKey = 'wuz14axWIJaDHRSx5gVUlC8KBXn9bVG_vtFOxxYOvuNxUAs7LQ6Mmfco' +
@@ -98,6 +105,52 @@ function restaurantApiRequest(term, latitude, longitude, location) {
     }).then((response) => {
         console.log(response);
         return response;
+    }).catch((err) => {
+        console.error(err);
+    })
+}
+
+/***
+ * https://www.yelp.com/developers/documentation/v3/business
+ ***/
+function restaurantByIdApiRequest(id) {
+    let clientID = 'rII8Y9mwHKyzKVlEWGQ2QA';
+    let apiKey = 'wuz14axWIJaDHRSx5gVUlC8KBXn9bVG_vtFOxxYOvuNxUAs7LQ6Mmfco' +
+        'Xyl48ynDRjJ3xnNuUM2to6uI617pdPG4AIcJMdBzh9cYF3zGV2Dm9we7zyNYZUH52QvIX3Yx';
+
+    let headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Bearer ' + apiKey
+    }
+
+    let url = 'https://api.yelp.com/v3/businesses/' +id;
+    console.log(url);
+
+    return fetch(url, {
+        method: 'GET',
+        headers: headers
+
+    }).then((response) => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            console.error(response);
+        }
+    }).then((response) => {
+        return {
+            id: response['id'],
+            alias: response['alias'],
+            name: response['name'],
+            image_url: response['image_url'],
+            is_closed: response['is_closed'],
+            url: response['url'],
+            display_phone:response['display_phone'],
+            categories: response['categories'],
+            rating: response['rating'],
+            location: response['location']['display_address'],
+            coordinates: response['coordinates'],
+            hours: response['hours'][0]['open']
+        };
     }).catch((err) => {
         console.error(err);
     })
