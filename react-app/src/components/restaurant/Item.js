724ad69b0351950502f758932ef8ec374286c688
woonicholas <nickj_woo@yahoo.com>
2020-12-12 20:38:33 -0800

    fix merge-conflict

--- react-app/src/components/restaurant/Item.js
+++ react-app/src/components/restaurant/Item.js
@@ -2,27 +2,27 @@ import React, { Component } from 'react';
 import '../../css/restaurant/item.style.css';
 // import {} from 'reactstrap';
 
-class Item extends Component {
-    render(){
-        return (
-            <div>
-                <div class = 'container ' >
-                    <div class = 'row pb-2 listContainer border rounded'>
-                        <div class = 'col-4'>
-                            <img src = {process.env.PUBLIC_URL + '/placeholder_img.jpg'} alt="food image" className = 'rounded p-2' id = 'listImage'/>
-                        </div>
-                        <div class = 'col-6'>
-                            <h2 class = "pt-2">Chicken House</h2>
-                            <p>Seafood. Mexican. Sushi Bar.</p> 
-                            <img src = 'images/location_img.png' class = "pb-1"/> 1 Forest, Irvine, CA 92612
-                            <p><img src = 'images/time_img.png'/> Open </p>
-                            <p>Rating: 4.5/5</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+function Item(props) {
+return (
+  <div>
+    <div class = 'container pb-2' >
+      <div class = 'row listContainer border rounded'>
+        <div class = 'col-4'>
+          <img src = "" alt="food image" className = 'rounded p-2' id = 'listImage'/>
+        </div>
+        <div class = 'col-6'>
+          <h2 class = "pt-2">{props.results.name}</h2>
+          <p>{props.results.categories.map((c,index) => (
+            c.title
+          ))}</p> 
+          <img src = 'images/location_img.png' class = "pb-1"/> 1 Forest, Irvine, CA 92612
+          <p><img src = 'images/time_img.png'/> Open </p>
+          <p>Rating: 4.5/5</p>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 }
 
 export default Item;
--- react-app/src/components/restaurant/ResultsRestaurant.js
+++ react-app/src/components/restaurant/ResultsRestaurant.js
@@ -2,13 +2,25 @@ import React, { Component } from 'react';
 import Item from './Item';
 import '../../css/restaurant/results.style.css';
 import {Label, FormGroup, Input, Button} from 'reactstrap';
+import { withRouter } from 'react-router-dom';
 
 class ResultsRestaurant extends Component {
     // constructor(){
     //     super();
     //     // this.dataSet = [<Item/>*500]
     // }
+    componentDidMount(){
+        console.log(this.props.location.state.results)
+    }
+
+    renderResults(){
+        if(this.props.location.state.results){
+            return  <Item results={this.props.location.state.results[0]}/>
+        }
+    }
+
     render(){
+
         return (
             <div>
                 <div className='container-fluid' >
@@ -27,14 +39,11 @@ class ResultsRestaurant extends Component {
                 </div>
                 <hr></hr>
                 <div className='row justify-content-center' >
-                    <Item/>
-                </div>
-                <div className='row justify-content-center' >
-                    <Item/>
+                   {this.renderResults}
                 </div>
             </div>
         )
     }
 }
 
-export default ResultsRestaurant;
+export default withRouter(ResultsRestaurant);
